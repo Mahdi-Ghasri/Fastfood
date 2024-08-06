4f89429bad20d787d4f86d893fb4c24ac8ebf020
Mahdigh1363 <mahdighasri4029@gmail.com>
2024-08-06 11:01:07 +0330

    create basic files

---
+++ tamrin.py
@@ -0,0 +1,12 @@
+def ageValidate(age):
+    if not isinstance(age,int):
+        raise RuntimeError ("Type is not valid...")
+    if age<1 or age>120:
+        raise RuntimeError ("Out of range...")
+    return age
+
+try:
+    age=ageValidate(39)
+    print(f"Age Is : {age}")
+except RuntimeError as error:
+    print (error)
